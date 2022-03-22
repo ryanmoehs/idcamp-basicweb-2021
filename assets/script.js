99d4cc76bde0d56c968ed0196345b1939d223a3a
Ryan Muhammad <66431118+ryanmoehs@users.noreply.github.com>
2022-03-22 08:46:27 +0700

    Add files via upload
    initial commit

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// $(document).on('click', '#comingSoon', function () {
+//   alert('Mohon maaf. Aplikasi masih dikembangkan');
+// });
+
+function comingSoon() {
+  alert('Mohon maaf, aplikasi masih dikembangkan');
+}
